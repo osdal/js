4b7eae38578d836b0534c2d6468bd2ed0457975b
osdal <osdal67@gmail.com>
2018-07-27 15:24:48 +0300

    финал

--- JS/script.js
+++ JS/script.js
@@ -1,72 +1,37 @@
-/*let number = 5;
-/*console.log (number);
-console.log ("ЭСтрока");*/
-/*alert("D[jl запрещен");*/
-/*let answeк = confirm("Вам 18?");*/
-/*let answeк = prompt("Вам 18?");
-console.log (answeк);*/
-/*
-let answer = [];
-
-let questions = [
-	"Как ваше имя?",
-	"Как ваша фамилия?",
-	"Сколько вам лет?"
-];
-
-for (let i = 0; i < questions.length; i++) {
-	answer[i] = prompt(questions[i])
-}
 
 
-document.write(answer);
-*/
 
-/*
-let age = prompt("Сколько лет?");
-
-if(age >18){
-	alert("ok")
-} else {
-	alert("no")
-}*/
-/*
-function hello (ob) {
-	// body... 
-	document.write("Hello " + ob +"!")
-}
-
-hello("Иван");
-hello("Дима");*/
-/*
-function calc (a,b) {
-	console.log(a+b)
-}*/
-
-calc(4,5);
 
 function myFirstApp(name,  age)  {
 	alert("Привет, меня зовут " + name + " и это моя первая программа");
 	function showSkills() {
-		let skills = [/*Мои навыки*/]
+		let skills = ["HTML", "CSS", "Wordpress", "Photoshop", "Drupal"]
 
 			for (let i = 0;  i < skills.length;   i++)  {
-				//я владею html <br>
+				document.write("Я владею " + skills[i] + "<br>")
 			}
 	}
 
 	showSkills();
 
+
+
 function checkage() {
+	 let age = prompt("Введите возраст")
 	 if (age > 18) {
-	 	//сообщение
+	 	alert("Можете войти")
 	 } else {
-	 	//
+	 	alert("Вам нужно подрасти")
 	 }
 } 
+checkage();
+
 
 function calcPow(num)  {
 	//написать функционал
+	
+	let num2 = num*num;
+	alert(num + " в квадрате = " +num2);
  }
 calcPow(4)
 }
