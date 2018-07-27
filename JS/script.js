1f00d5019526fd3ee4f71e5c1b8f46c1abdd7640
osdal <osdal67@gmail.com>
2018-07-27 14:28:00 +0300

    Начало написания

---
+++ JS/script.js
@@ -0,0 +1,75 @@
+/*let number = 5;
+/*console.log (number);
+console.log ("ЭСтрока");*/
+/*alert("D[jl запрещен");*/
+/*let answeк = confirm("Вам 18?");*/
+/*let answeк = prompt("Вам 18?");
+console.log (answeк);*/
+/*
+let answer = [];
+
+let questions = [
+	"Как ваше имя?",
+	"Как ваша фамилия?",
+	"Сколько вам лет?"
+];
+
+for (let i = 0; i < questions.length; i++) {
+	answer[i] = prompt(questions[i])
+}
+
+
+document.write(answer);
+*/
+
+/*
+let age = prompt("Сколько лет?");
+
+if(age >18){
+	alert("ok")
+} else {
+	alert("no")
+}*/
+/*
+function hello (ob) {
+	// body... 
+	document.write("Hello " + ob +"!")
+}
+
+hello("Иван");
+hello("Дима");*/
+/*
+function calc (a,b) {
+	console.log(a+b)
+}*/
+
+calc(4,5);
+
+function myFirstApp(name,  age)  {
+	alert("Привет, меня зовут " + name + " и это моя первая программа");
+	function showSkills() {
+		let skills = [/*Мои навыки*/]
+
+			for (let i = 0;  i < skills.length;   i++)  {
+				//я владею html <br>
+			}
+	}
+
+	showSkills();
+
+function checkage() {
+	 if (age > 18) {
+	 	//сообщение
+	 } else {
+	 	//
+	 }
+} 
+
+function calcPow(num)  {
+	//написать функционал
+ }
+calcPow(4)
+}
+
+myFirstApp("Ivan", 32)
+
